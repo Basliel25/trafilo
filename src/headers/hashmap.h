1393b438105338900eda43c1a9b0e6a59ec5886c
Ras Bas <basgug25@gmail.com>
2026-05-07 12:35:41 +0200

    Created header for the hashtable

--- src/headers/hashmap.h
+++ src/headers/hashmap.h
@@ -9,3 +9,58 @@
  * chaining for collisions
  * FNV1-a hasing used for key (service) names
  */
+#ifndef HASMAP_H
+#define HASMAP_H
+
+#include <pthread.h>
+#include <time.h>
+#include "window.h"
+#include "../../include/trafilo.h"
+
+/**
+ * @brief A bucket node of the hashtable
+ */
+typedef struct bucket_node {
+    char *key; /* Bucket owned key*/
+    void *state; /* User defined state */
+    sliding_window_t window; /* An embedded sliding window */
+
+    pthread_mutex_t bucket_lock; /* Mutex lock for bucket */
+
+    struct bucket_node_t *next;
+    struct timespec last_event_ts;
+} bucket_node;
+
+/**
+ * @brief The hasmap.
+ */
+typedef struct hashmap_t {
+    bucket_node **buckets; /* Array of chain buckets */
+    size_t num_buckets; /* Number of buckets in hasmap */
+} hashmap_t;
+
+/**
+ * @brief Create a hashmap
+ * @param size_t num_buckets: Number of buckets
+ */
+hashmap_t *hasmap_create(size_t num_buckets);
+
+/**
+ * @brief Graceful Cleanup
+ * @param hasmap_t * pointer to the hashmap
+ * @param trafilo_state_free_fn user specified state_free function
+ */
+void hasmap_destroy(hashmap_t *hashmap, trafilo_state_free_fn state_free);
+
+/**
+ * @brief For each iteration for operations
+ *        calls passed function within lock protection
+ * @param hasmap_t * pointer to the hashmap
+ * @param void (*fn) function with bucket_node and arguments
+ * @param void *arg arguments for the iterative function
+ */
+void hashmap_for_each(hashmap_t *hashmap, 
+        void (*fn)(bucket_node *bucket, void *arg), 
+        void *arg);
+
+#endif
