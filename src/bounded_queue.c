0981644ff67e687feda50b6f64d19a008d37a540
Ras Bas <basgug25@gmail.com>
2026-05-07 13:22:57 +0200

    Implemented queue push function

--- src/bounded_queue.c
+++ src/bounded_queue.c
@@ -1,5 +1,5 @@
 #include "headers/bounded_queue.h"
-
+#include <pthread.h>
 
 bounded_queue_t *bq_create(size_t capacity) {
     bounded_queue_t *bounded_q;
@@ -28,9 +28,56 @@ bounded_queue_t *bq_create(size_t capacity) {
     bounded_q->done = 0;
     return bounded_q;
 }
-void bq_destroy(bounded_queue_t *q);
 
-int bq_push(bounded_queue_t *q, void *item);    
+void bq_destroy(bounded_queue_t *q) {
+    pthread_mutex_destroy(&q->mu_lock);
+    pthread_cond_destroy(&q->not_empty);
+    pthread_cond_destroy(&q->not_full);
+    
+    free(q->buf);
+    free(q);
+}
+
+int bq_push(bounded_queue_t *q, void *item) {
+    pthread_mutex_lock(&q->mu_lock);
+    //If full wait for not full signal to push
+    while(!q->done && q->count == q->capacity) {
+        pthread_cond_wait(&q->not_full, &q->mu_lock);
+    }
+    
+    // If done is flagged, unlock and return 
+    if(q->done) {
+        pthread_mutex_unlock(&q->mu_lock);
+        return -1;
+    }
+
+    // If pushing is still valid, append 
+    q->buf[q->tail] = item;
+    q->tail = (q->tail + 1) % q->capacity;
+    q->count += 1;
+    pthread_cond_signal(&q->not_empty);
+
+    pthread_mutex_unlock(&q->mu_lock);
+
+    return 0;
+}
+
 void *bq_pop(bounded_queue_t *q);                
-void bq_shutdown(bounded_queue_t *q);            
+
+void bq_shutdown(bounded_queue_t *q) {
+    // Acqure lock and set flag
+    pthread_mutex_lock(&q->mu_lock);
+    if(q->done) {
+        pthread_mutex_unlock(&q->mu_lock);
+    }
+    q->done = 1;
+
+    // Wake up all listening workers
+    pthread_cond_broadcast(&q->not_empty);
+    
+    // Wake up all listening producers
+    pthread_cond_broadcast(&q->not_empty);
+
+    pthread_mutex_unlock(&q->mu_lock);
+}            
 
